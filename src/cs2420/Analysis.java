22271f0470f3f946bd6a07ffe49c372c96ebdda5
ky-grr <kyl.flu@gmail.com>
2017-04-13 17:01:18 -0600

    Update Analysis.java

--- src/cs2420/Analysis.java
+++ src/cs2420/Analysis.java
@@ -14,62 +14,93 @@ public class Analysis {
 
 	public static void main(String[] args) {
 		
-		//Repetition loop for insertion.
-		for(int numberOfElements = 10000; numberOfElements<= 300000; numberOfElements += 10000) {
+//		//Repetition loop for insertion.
+//		for(int numberOfElements = 10000; numberOfElements<= 300000; numberOfElements += 10000) {
+//			
+//			Heap<Integer> heap = new Heap<>();
+//			Random generator = new Random();
+//			
+//			long startInsertion = System.nanoTime();
+//
+//			//Add all of the elements.
+//			for(int index=0; index<numberOfElements; index++) {
+//				heap.add(generator.nextInt(5000));
+//			}
+//			
+//			long totalInsertion = (System.nanoTime() - startInsertion) / numberOfElements;
+//						
+//			System.out.println("Stats for " + numberOfElements + " (insertion, bubble ups)");
+//			System.out.println(totalInsertion);
+//			System.out.println(heap.numberOfBubbleUps);
+//			
+//		}
+//		
+//		//Separate loop for the dequeue function.
+//		for(int numberOfElements=50000; numberOfElements<=1000000; numberOfElements+=50000) {
+//			
+//			Heap<Integer> heap = new Heap<>();
+//			Random generator = new Random();
+//			
+//			long startDeletion = 0;
+//			long totalDeletion = 0;
+//			
+//			for(int averagingCount=0; averagingCount<25; averagingCount++) {
+//				
+//				//Add all of the elements.
+//				for(int index=0; index<numberOfElements; index++) {
+//					heap.add(generator.nextInt(5000));
+//				}
+//				
+//				startDeletion = System.nanoTime();
+//				
+//				//Delete all of the elements.
+//				for(int index=0; index<numberOfElements; index++) {
+//					heap.dequeue();
+//				}
+//				
+//				heap.clear();
+//				
+//				totalDeletion += (System.nanoTime() - startDeletion) / numberOfElements;
+//				
+//			}
+//			
+//			//To average out all of the averaging runs.
+//			totalDeletion /= 25;
+//			
+//			System.out.println("Stats for " + numberOfElements);
+//			System.out.println(totalDeletion);
+//
+//		}
+		
+		//Separate loop for build_heap_from_array.
+		for(int numberOfElements = 10000; numberOfElements<=300000; numberOfElements+=10000) {
 			
-			Heap<Integer> heap = new Heap<>();
 			Random generator = new Random();
 			
-			long startInsertion = System.nanoTime();
-
-			//Add all of the elements.
+			long startBuild = 0;
+			long totalBuild = 0;
+			
+			//Build the array.
+			Integer[] array = new Integer[numberOfElements];
+			
 			for(int index=0; index<numberOfElements; index++) {
-				heap.add(generator.nextInt(5000));
+				array[index] = generator.nextInt(1000);
 			}
 			
-			long totalInsertion = (System.nanoTime() - startInsertion) / numberOfElements;
-						
-			System.out.println("Stats for " + numberOfElements + " (insertion, bubble ups)");
-			System.out.println(totalInsertion);
-			System.out.println(heap.numberOfBubbleUps);
-			
-		}
-		
-		//Separate loop for the dequeue function.
-		for(int numberOfElements=50000; numberOfElements<=1000000; numberOfElements+=50000) {
-			
-			Heap<Integer> heap = new Heap<>();
-			Random generator = new Random();
-			
-			long startDeletion = 0;
-			long totalDeletion = 0;
-			
+			//Averaging loop.
 			for(int averagingCount=0; averagingCount<25; averagingCount++) {
 				
-				//Add all of the elements.
-				for(int index=0; index<numberOfElements; index++) {
-					heap.add(generator.nextInt(5000));
-				}
+				Heap<Integer> heap = new Heap<>();
 				
-				startDeletion = System.nanoTime();
-				
-				//Delete all of the elements.
-				for(int index=0; index<numberOfElements; index++) {
-					heap.dequeue();
-				}
-				
-				heap.clear();
-				
-				totalDeletion += (System.nanoTime() - startDeletion) / numberOfElements;
+				startBuild = System.nanoTime();
+				heap.build_heap_from_array(array);
+				totalBuild += System.nanoTime() - startBuild;
 				
 			}
 			
-			//To average out all of the averaging runs.
-			totalDeletion /= 25;
+			System.out.println("Number of Elements\n" + numberOfElements);
+			System.out.println(totalBuild/25);
 			
-			System.out.println("Stats for " + numberOfElements);
-			System.out.println(totalDeletion);
-
 		}
 
 	}
